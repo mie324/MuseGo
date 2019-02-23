5ce91c2b207289391b0e09c06dd1e9dc497c78f7
keren0908 <akayayy@gmail.com>
2019-02-23 10:36:59 -0500

    add tour activity

---
+++ app/src/main/java/com/ece1778/musego/UI/Tour/TourDetailActivity.java
@@ -0,0 +1,16 @@
+package com.ece1778.musego.UI.Tour;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ece1778.musego.BaseActivity;
+import com.ece1778.musego.R;
+
+public class TourDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tour_detail);
+    }
+}
---
+++ app/src/main/java/com/ece1778/musego/UI/Tour/TourListActivity.java
@@ -0,0 +1,8 @@
+package com.ece1778.musego.UI.Tour;
+
+import com.ece1778.musego.BaseActivity;
+
+public class TourListActivity extends BaseActivity {
+
+    // recyclerview here
+}
