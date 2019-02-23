4354a607bd26c377894a7f1ce00303fa2f48e40b
keren0908 <akayayy@gmail.com>
2019-02-23 10:31:06 -0500

    add auth UI

---
+++ app/src/main/java/com/ece1778/musego/UI/Auth/SiginupActivity.java
@@ -0,0 +1,19 @@
+package com.ece1778.musego.UI.Auth;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ece1778.musego.BaseActivity;
+import com.ece1778.musego.R;
+
+public class SiginupActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+    }
+}
---
+++ app/src/main/java/com/ece1778/musego/UI/Auth/SigninActivity.java
@@ -0,0 +1,20 @@
+package com.ece1778.musego.UI.Auth;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ece1778.musego.BaseActivity;
+import com.ece1778.musego.R;
+
+public class SigninActivity extends BaseActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+
+    }
+}
