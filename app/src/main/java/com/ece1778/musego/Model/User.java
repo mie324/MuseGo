3f31ea63d6b9cb5877f1135ed0b89cc516d6333e
Ivana <ivana.zhangyf@gmail.com>
2019-03-14 22:08:31 -0400

    Seperate pro && public

--- app/src/main/java/com/ece1778/musego/Model/User.java
+++ app/src/main/java/com/ece1778/musego/Model/User.java
@@ -5,15 +5,17 @@ public class User {
     private String username;
     private String avatar;
     private String bio;
+    private int role;
 
     public User(){
 
     }
 
-    public User(String username, String avatar, String bio) {
+    public User(String username, String avatar, String bio, int role) {
         this.username = username;
         this.avatar = avatar;
         this.bio = bio;
+        this.role = role;
     }
 
     public String getUsername() {
@@ -38,5 +40,13 @@ public class User {
 
     public void setBio(String bio) {
         this.bio = bio;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
     }
 }
